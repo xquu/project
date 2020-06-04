85f0e2b7cdae4c325e22dcb2e707bc05c591bee0
aloss <793508013>
2020-06-04 16:28:16 +0800

    删除主项目src文件夹 添加eureka子模块

--- eureka/src/main/java/cn/xquu/eureka/ProjectApplication.java
+++ eureka/src/main/java/cn/xquu/eureka/ProjectApplication.java
@@ -1,13 +1,11 @@
-package cn.xquu.project;
+package cn.xquu.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
-
 }
