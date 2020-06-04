041fc5798bd81535e8bb572e97c0481e56fd3427
aloss <793508013>
2020-06-04 17:20:20 +0800

    删除临时文件

---
+++ eureka/src/main/java/cn/xquu/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.xquu.eureka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    private static final Logger log = LoggerFactory.getLogger(EurekaApplication.class);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(EurekaApplication.class, args);
+        log.info("项目启动成功！");
+        log.info("eureka地址:\t http://localhost:{}"+context.getEnvironment().getProperty("server.port"));
+
+    }
+}
