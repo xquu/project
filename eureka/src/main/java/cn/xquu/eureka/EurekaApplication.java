7c92d102c98b36816471e8e0fce036bb034328af
aloss <793508013>
2020-06-05 16:16:08 +0800

    1.解决注册中心启动失败问题 2.优化日志输出

--- eureka/src/main/java/cn/xquu/eureka/EurekaApplication.java
+++ eureka/src/main/java/cn/xquu/eureka/EurekaApplication.java
@@ -13,8 +13,7 @@ public class EurekaApplication {
     private static final Logger log = LoggerFactory.getLogger(EurekaApplication.class);
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(EurekaApplication.class, args);
-        log.info("项目启动成功！");
-        log.info("eureka地址:\t http://localhost:{}"+context.getEnvironment().getProperty("server.port"));
+        log.info("项目启动成功！eureka地址:\t http://localhost:{}",context.getEnvironment().getProperty("server.port"));
 
     }
 }
