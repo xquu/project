17349baa65447e47ea8a74466b8602062c4ef0ca
aloss <793508013>
2020-06-09 17:30:22 +0800

    1.增加路由模块geteway，并调试成功

--- geteway/src/main/java/cn/xquu/project/geteway/GetewayApplication.java
+++ geteway/src/main/java/cn/xquu/project/geteway/GetewayApplication.java
@@ -1,4 +1,4 @@
-package cn.xquu.project.system;
+package cn.xquu.project.geteway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +9,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemApplication {
-    private static final Logger log = LoggerFactory.getLogger(SystemApplication.class);
+public class GetewayApplication {
+    private static final Logger log = LoggerFactory.getLogger(GetewayApplication.class);
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SystemApplication.class, args);
-        log.info("项目启动成功！system地址:\t http://localhost:{}/system",context.getEnvironment().getProperty("server.port"));
+        ConfigurableApplicationContext context = SpringApplication.run(GetewayApplication.class, args);
+        log.info("项目启动成功！eureka地址:\t http://localhost:{}",context.getEnvironment().getProperty("server.port"));
 
     }
 }
