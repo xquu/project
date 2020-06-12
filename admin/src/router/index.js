36632933aa058b824f00fa33c30fcb0500a04b7c
xquu <793508013@qq.com>
2020-06-12 20:45:57 +0800

    优化日志，新增chapter路由并加入table

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -7,6 +7,7 @@ import Router from 'vue-router'
 const Login = () => import("@/views/login.vue")
 const Admin = () => import("@/views/admin.vue")
 const Welcome = () => import("@/views/admin/welcome.vue")
+import Chapter from "../views/admin/chapter"
 //import Admin from "@/views/admin.vue"
 // Register Vue Router
 Vue.use(Router)
@@ -14,8 +15,7 @@ Vue.use(Router)
 // Router Configuration
 export default new Router({
     mode: 'history',
-    routes: [
-        {
+    routes: [{
             path: "*",
             redirect: "/login",
         }, {
@@ -30,6 +30,9 @@ export default new Router({
             children:[{
                 path: "welcome",
                 component: Welcome
+            },{
+                path: "chapter",
+                component: Chapter
             }]
         }
     ]
