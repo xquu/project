9541602a28a243e1ea90cb864680f4b8adadc227
aloss <793508013>
2020-06-28 17:00:32 +0800

    更新测试语句

--- document/database/all.sql
+++ document/database/all.sql
@@ -6,5 +6,11 @@ create table `chapter` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='大章';
 
-insert into `chapter` (id,course_id,name) values ('00000000', '00000000','测试大章一');
-insert into `chapter` (id,course_id,name) values ('00000001 ','00000000','测试大章二');
+insert into `chapter` (id,course_id,name) values ('00000000', '00000000','测试大章0');
+insert into `chapter` (id,course_id,name) values ('00000001 ','00000001','测试大章1');
+insert into `chapter` (id,course_id,name) values ('00000002', '00000002','测试大章2');
+insert into `chapter` (id,course_id,name) values ('00000003 ','00000003','测试大章3');
+insert into `chapter` (id,course_id,name) values ('00000004', '00000004','测试大章4');
+insert into `chapter` (id,course_id,name) values ('00000005 ','00000005','测试大章5');
+insert into `chapter` (id,course_id,name) values ('00000006', '00000006','测试大章6');
+insert into `chapter` (id,course_id,name) values ('00000007 ','00000007','测试大章7');
